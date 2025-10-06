47d3d9594c8ad8cf791631d32eba19b8a3493d70
akmaralAW <moldagalieva@tu-berlin.de>
2025-10-06 10:24:51 +0100

    run benchmark for h estimation with est

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -99,13 +99,13 @@ def main():
     # "circle6_integrator",
     # "circle8_integrator",
     # "circle10_integrator",
-    # "circle2_unicycle",
-    # "circle4_unicycle",
-    # "circle6_unicycle",
-    # "circle8_unicycle",
-    # "circle10_unicycle",
-    # "alcove_unicycle",
-    # "atgoal_unicycle",
+    "circle2_unicycle",
+    "circle4_unicycle",
+    "circle6_unicycle",
+    "circle8_unicycle",
+    "circle10_unicycle",
+    "alcove_unicycle",
+    "atgoal_unicycle",
     # 3D case
     # "forest4",
     # "corridor4",
@@ -120,10 +120,10 @@ def main():
     # "test_n40_0_unicycle",
     # "test_n50_0_unicycle",
   ]
-  for kind in ["unicycle_sphere"]: #"unicycle",
-    for n in [8]:
-      for k in range(10):
-        instances.append("gen_p10_n{}_{}_{}".format(n,k, kind))
+  # for kind in ["unicycle_sphere"]: #"unicycle",
+  #   for n in [8]:
+  #     for k in range(10):
+  #       instances.append("gen_p10_n{}_{}_{}".format(n,k, kind))
 
   instances_n = ["test_n10_0_unicycle", "test_n20_0_unicycle", "test_n30_0_unicycle", "test_n40_0_unicycle", "test_n50_0_unicycle"] 
 
